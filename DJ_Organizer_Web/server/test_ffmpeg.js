b40868324165eab20a56b92d68e1d2936af04785
Theimbold <104437311+Theimbold@users.noreply.github.com>
2026-02-04 20:37:39 +0100

    Delete_Verschachtelung

--- DJ_Organizer_Web/server/test_ffmpeg.js
+++
@@ -1,8 +0,0 @@
-const ffmpeg = require('fluent-ffmpeg');
-
-try {
-    const command = ffmpeg();
-    console.log('ffmpeg is accessible.');
-} catch (error) {
-    console.error('Error accessing ffmpeg:', error.message);
-}
