29c55d2de3d9b6ad9d438d2ef354f2e808516f28
Theimbold <104437311+Theimbold@users.noreply.github.com>
2026-02-07 12:28:32 +0100

    tailwind_anpassung

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,18 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        primary: '#ff006e',
-        secondary: '#3A86FF',
-      }
-    },
+  colors: {
+    primary: '#ff006e',
+    secondary: '#3A86FF',
+
+    textPrimary: '#f8fafc',   // fast weiß
+    textSecondary: '#cbd5e1', // hellgrau
+    textMuted: '#94a3b8',     // meta infos
+
+    surface: '#111111',
+    surfaceSoft: '#1c1c1c',
+  }
+},
   },
   plugins: [],
 }
